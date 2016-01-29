d5a8981a020bcb3a3cf2f04db6a243bb32a62683
Jordan Harrop <jordanharrop@me.com>
2016-01-29 14:54:43 -0500

    Update Model Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,9 @@ class Product < ActiveRecord::Base
   validates_presence_of :style_number, :category_id, :mobility_id, :gender, :image_url
   validates_uniqueness_of :style_number, :confirmation_number
   validates_numericality_of :mobility_rank, only_integer: true
+  validate :has_product_markets
+
+  def has_product_markets
+    errors.add(:market_ids, 'Product must have at least one market.') if self.locations.blank?
+  end
 end
